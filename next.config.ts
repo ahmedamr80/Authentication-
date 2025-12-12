e37a7e52eef5aa2729d33f2b85c20f675d912d40
ahmedamr80 <ahmedamr80@gmail.com>
2025-12-13 03:37:00 +0400

    fix: resolve deployment issues and upgrade dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,49 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      // 2. Google User Content (for Google Sign-In profiles)
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
       },
       {
-        protocol: "https",
-        hostname: "**",
+        protocol: 'https',
+        hostname: '**',
+      },
+      // 3. The Luxury Network
+      {
+        protocol: 'https',
+        hostname: 'www.theluxurynetwork.ae',
+        port: '',
+        pathname: '/**',
+      },
+      // 4. Media Office AE
+      {
+        protocol: 'https',
+        hostname: 'www.mediaoffice.ae',
+        port: '',
+        pathname: '/**',
+      },
+      // 5. Shopify CDN
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+        port: '',
+        pathname: '/**',
+      },
+      // 6. Padel Magazine
+      {
+        protocol: 'https',
+        hostname: 'padelmagazine.fr',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
